99eeed5b64e7a5676a39828b6b3041406ec318a2
Ron Roth Jr <ronrothjr@gmail.com>
2022-02-19 13:28:09 -0500

    scroller fixes

--- tutorials/scroller/scroller.py
+++ tutorials/scroller/scroller.py
@@ -1,7 +1,10 @@
 #pylint:disable=E0602
-import pygame, sys
+import pygame, sys, os, random
 from pygame.locals import *
-import random
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
  
 pygame.init()
  
@@ -17,7 +20,7 @@ WHITE = (255, 255, 255)
 info = pygame.display.Info()
 SCREEN_WIDTH = info.current_w
 SCREEN_HEIGHT = info.current_h
-DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME | pygame.FULLSCREEN | pygame.RESIZABLE)
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
  
@@ -43,7 +46,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__() 
         self.image = pygame.image.load("player.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+        self.rect.center = (int(SCREEN_WIDTH / 2), SCREEN_HEIGHT - 200)
  
     def update(self):
         pressed_keys = pygame.key.get_pressed()
@@ -56,7 +59,7 @@ class Player(pygame.sprite.Sprite):
               if pressed_keys[K_LEFT] or pressed_keys[K_a]:
                   self.rect.move_ip(-5, 0)
         if self.rect.right < SCREEN_WIDTH:        
-              if pressed_keys[K_RIGHT] or pressed_keys[K_l]:
+              if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
                   self.rect.move_ip(5, 0)
  
     def draw(self, surface):
@@ -64,19 +67,26 @@ class Player(pygame.sprite.Sprite):
  
          
 P1 = Player()
-E1 = Enemy()
+ENEMIES = 5
+SPAWNTIME = 6000
+last_spawn_tick = 0
+enemies = []
  
 while True:     
     for event in pygame.event.get():              
-        if event.type == QUIT:
+        if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.quit()
             sys.exit()
     P1.update()
-    E1.move()
+    now = pygame.time.get_ticks()
+    if now - last_spawn_tick >= SPAWNTIME:
+        last_spawn_tick = pygame.time.get_ticks()
+        enemies.append(Enemy())
+    [e.move() for e in enemies]
      
     DISPLAYSURF.fill(WHITE)
     P1.draw(DISPLAYSURF)
-    E1.draw(DISPLAYSURF)
+    [e.draw(DISPLAYSURF) for e in enemies]
          
     pygame.display.update()
     FramePerSec.tick(FPS)
