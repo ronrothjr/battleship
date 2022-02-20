9760655b0671e27395616793c2c280ebbabc74b8
Ron Roth Jr <ronrothjr@gmail.com>
2022-02-19 21:13:29 -0500

    scroller with scrolling bg, score, sound, collision, and game over

--- tutorials/scroller/scroller.py
+++ tutorials/scroller/scroller.py
@@ -1,5 +1,5 @@
 #pylint:disable=E0602
-import pygame, sys, os, random
+import pygame, sys, os, random, time
 from pygame.locals import *
 
 abspath = os.path.abspath(__file__)
@@ -20,9 +20,36 @@ WHITE = (255, 255, 255)
 info = pygame.display.Info()
 SCREEN_WIDTH = info.current_w
 SCREEN_HEIGHT = info.current_h
+SPEED = 5
+SCORE = 0
+
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+background = pygame.image.load("AnimatedStreet.png")
+ 
+def makeTiledImage( image, width, height ):
+    x_cursor = 0
+    y_cursor = 0
+
+    tiled_image = pygame.Surface( ( width, height + (164 * 2) ) )
+    while ( y_cursor < height + image.get_height() ):
+        while ( x_cursor < width ):
+            tiled_image.blit( image, ( x_cursor, y_cursor ) )
+            x_cursor += image.get_width()
+        y_cursor += image.get_height()
+        x_cursor = 0
+    return tiled_image
+
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME | pygame.FULLSCREEN | pygame.RESIZABLE)
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
+
+tile_image = pygame.image.load('pavement_tile.png').convert_alpha()
+background = makeTiledImage( tile_image, SCREEN_WIDTH - 86, SCREEN_HEIGHT )
+left_shoulder = pygame.image.load('left_shoulder.png').convert_alpha()
+right_shoulder = pygame.image.load('right_shoulder.png').convert_alpha()
+tile_height = 164
  
 class Enemy(pygame.sprite.Sprite):
       def __init__(self):
@@ -32,13 +59,12 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0) 
  
       def move(self):
-        self.rect.move_ip(0,10)
+        global SCORE
+        self.rect.move_ip(0, SPEED)
         if (self.rect.bottom > SCREEN_HEIGHT):
+            SCORE += 1
             self.rect.top = 0
             self.rect.center = (random.randint(30, SCREEN_WIDTH-30), 0)
- 
-      def draw(self, surface):
-        surface.blit(self.image, self.rect) 
  
  
 class Player(pygame.sprite.Sprite):
@@ -48,7 +74,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (int(SCREEN_WIDTH / 2), SCREEN_HEIGHT - 200)
  
-    def update(self):
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
        #if pressed_keys[K_UP]:
             #self.rect.move_ip(0, -5)
@@ -61,32 +87,58 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right < SCREEN_WIDTH:        
               if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
                   self.rect.move_ip(5, 0)
- 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)     
- 
          
 P1 = Player()
-ENEMIES = 5
 SPAWNTIME = 6000
 last_spawn_tick = 0
-enemies = []
- 
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+
+tile_placement = 164 * -1
+
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 5000)
+
 while True:     
-    for event in pygame.event.get():              
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+              SPEED += 2            
         if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.quit()
             sys.exit()
-    P1.update()
     now = pygame.time.get_ticks()
     if now - last_spawn_tick >= SPAWNTIME:
         last_spawn_tick = pygame.time.get_ticks()
-        enemies.append(Enemy())
-    [e.move() for e in enemies]
-     
+        enemy = Enemy()
+        enemies.add(enemy)
+        all_sprites.add(enemy)
     DISPLAYSURF.fill(WHITE)
-    P1.draw(DISPLAYSURF)
-    [e.draw(DISPLAYSURF) for e in enemies]
+    DISPLAYSURF.blit(left_shoulder, (0,0))
+    DISPLAYSURF.blit(background, (41, tile_placement))
+    tile_placement += 5
+    if tile_placement > 0:
+        tile_placement = 164 * -1
+    DISPLAYSURF.blit(right_shoulder, (SCREEN_WIDTH - 45,0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURF.blit(scores, (10,10))
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('crash.wav').play()
+        time.sleep(0.5)
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, ( int(SCREEN_WIDTH / 2) - 150, int(SCREEN_HEIGHT / 2) - 80) )
+        final_score = font.render(f"Final Score: {SCORE}", True, BLACK)
+        DISPLAYSURF.blit(final_score, ( int(SCREEN_WIDTH / 2) - 200, int(SCREEN_HEIGHT / 2) + 100) )
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill() 
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()  
+     
          
     pygame.display.update()
     FramePerSec.tick(FPS)
